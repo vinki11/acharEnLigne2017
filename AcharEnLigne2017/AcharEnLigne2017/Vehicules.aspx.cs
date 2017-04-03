4fa57eeeb24b22b44839f529dae1e4d2f7407877
Pier-Luc <p.ducharme@live.fr>
2017-04-03 13:28:39 -0400

    page vehicules fonctionne comme une championne

--- AcharEnLigne2017/AcharEnLigne2017/Vehicules.aspx.cs
+++ AcharEnLigne2017/AcharEnLigne2017/Vehicules.aspx.cs
@@ -15,41 +15,32 @@ public partial class Vehicules : System.Web.UI.Page
     protected void ajouterPanier(GridViewRow row)
     {
         string nom = ((Label)row.Cells[0].FindControl("labelNom")).Text;
-        int qte = Int32.Parse(((TextBox)row.Cells[3].FindControl("textBoxQte")).Text);
-        //Si la quantitée > 0 on fait le traitement
-        if (qte > 0)
+        int qte = 1;
+
+        float prix = Single.Parse(((Label)row.Cells[1].FindControl("labelPrix")).Text);
+        float totalPrix = prix * qte;
+        List<String> listePanier = new List<string>();
+        //Response.Write("<script type='text/javascript'>alert('" + ((Label)row.Cells[4].FindControl("labelPrix")).Text.Substring(1) + "');</script>");
+
+        //Trouver session si elle existe pui ajouter item dans panier
+        if (Session["liste_panier"] != null)
         {
-            float prix = Single.Parse(((Label)row.Cells[1].FindControl("labelPrix")).Text);
-            float totalPrix = prix * qte;
-            List<String> listePanier = new List<string>();
-            //Response.Write("<script type='text/javascript'>alert('" + ((Label)row.Cells[4].FindControl("labelPrix")).Text.Substring(1) + "');</script>");
-
-            //Trouver session si elle existe pui ajouter item dans panier
-            if (Session["liste_panier"] != null)
-            {
-                listePanier = (List<String>)Session["liste_panier"];
-            }
-
-            //Trouver index dans le panier, si il existe update la qte, sinon ajouter produit
-            int index = indexPanier(listePanier, nom);
-            if (index != -1)
-            {
-                int ancienneQte = Int32.Parse(listePanier[index].Split('&')[1]);
-                qte += ancienneQte;
-                totalPrix = prix * qte;
-                listePanier[index] = nom + "&" + qte + "&" + totalPrix;
-            }
-            else
-            {
-                listePanier.Add(nom + "&" + qte + "&" + totalPrix);
-            }
-
-            //Remettre la textBox de qte à 0 après l'ajout
-            ((TextBox)row.Cells[3].FindControl("textBoxQte")).Text = "0";
-
-            Session["liste_panier"] = listePanier;
+            listePanier = (List<String>)Session["liste_panier"];
         }
 
+        //Trouver index dans le panier, si il existe update la qte, sinon ajouter produit
+        int index = indexPanier(listePanier, nom);
+        if (index != -1)
+        {
+            totalPrix = prix * qte;
+            listePanier[index] = nom + "&" + qte + "&" + totalPrix;
+        }
+        else
+        {
+            listePanier.Add(nom + "&" + qte + "&" + totalPrix);
+        }
+
+        Session["liste_panier"] = listePanier;
     }
 
     protected void buttonAddPanier_Click(object sender, EventArgs e)
